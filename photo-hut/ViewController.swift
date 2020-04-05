436f9fb612202b6d6ced0a862e342b481658daad
hredoy <hredoy@hredoys-Mac.local>
2020-04-04 18:50:33 -0700

    Initial Commit

---
+++ photo-hut/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  photo-hut
+//
+//  Created by hredoy on 4/4/20.
+//  Copyright © 2020 hredoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
